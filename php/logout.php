21f824701a065a6e5d4eed35a6658ebfe56c0aea
ymoricode <129699138+ymoricode@users.noreply.github.com>
2024-06-09 00:57:53 +0700

    Add files via upload

---
+++ php/logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if (isset($_SESSION['sesLogin'])) {
+    unset($_SESSION);
+    session_destroy();
+    echo "<h1>Anda sudah berhasil LOGOUT</h1>";
+    echo "<h2>Klik <a href='session01.php'>disini</a> untuk LOGIN kembali</h2>";
+    echo "<h2>Anda sekarang tidak bisa masuk ke halaman <a href='session02.php'>session02.php</a> Lagi</h2>";
+}
